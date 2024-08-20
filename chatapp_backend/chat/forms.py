7ff4a96260d6332fc12525bfcee319618b3ddc88
andi <andy.weitzel99@gmail.com>
2024-08-20 21:50:58 +0200

    without venv

--- chatapp_backend/chat/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.forms import ModelForm
-from .models import MyUser
-
-class UserCreateForm(UserCreationForm):
-    class Meta(UserCreationForm):
-        model = MyUser
-        fields = ('email', 'username', 'password1', 'password2')
--- chatapp_frontend/android/app/src/main/kotlin/com/example/chatapp_frontend/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.chatapp_frontend
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
